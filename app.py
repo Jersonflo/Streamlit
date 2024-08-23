3155132c32ab4b0bb3c52d290f382d2f339d3e80
EnigmaEcho <jgiraldof@unal.edu.co>
2024-08-23 09:30:20 -0500

    update

--- app.py
+++ app.py
@@ -1,41 +1,23 @@
 import streamlit as st
-import cv2
-import numpy as np
+import imageio
+import PIL.Image
 
 def main():
-    st.title("Video Capture with OpenCV and Streamlit")
+    st.title("Video Capture with imageio and Streamlit")
 
-    # Selección de la cámara
-    cam_index = st.sidebar.selectbox("Seleccione la cámara", options=[0, 1, 2, 3], index=0)
-
-    # Configuración de la cámara
-    cap = cv2.VideoCapture(cam_index)
-
-    if not cap.isOpened():
-        st.error("No se pudo acceder a la cámara.")
-        return
+    # Configurar el dispositivo de la cámara
+    camera = imageio.get_reader('<video0>')
 
     stframe = st.empty()
 
-    # Loop de captura de video
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            st.error("No se pudo leer el frame de la cámara.")
-            break
+        frame = camera.get_next_data()
+        frame = PIL.Image.fromarray(frame)
 
-        # Convertir el frame a formato compatible con Streamlit (RGB)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Mostrar el frame en Streamlit
         stframe.image(frame)
 
-        # Salir del loop cuando el usuario cierre la aplicación
         if st.sidebar.button("Cerrar"):
             break
-
-    # Liberar la cámara
-    cap.release()
 
 if __name__ == "__main__":
     main()
